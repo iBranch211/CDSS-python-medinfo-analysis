6e076437e617039dbb650bd134afefea52cdf928
iBranch211 <leo.esaki211@gmail.com>
2017-09-18 16:21:04 -0700

    Move repo from SVN to Git

---
+++ medinfo/cpoe/analysis/Util.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""Miscellaneous utility functions used across the application
+"""
+import Const, Env
+import sys, os
+import logging
+
+log = logging.getLogger("CDSS")
+# log.setLevel(Const.LOGGER_LEVEL)
+
+handler = logging.StreamHandler(sys.stderr)
+formatter = logging.Formatter(Const.LOGGER_FORMAT)
+
+handler.setFormatter(formatter)
+log.addHandler(handler)
---
+++ medinfo/db/Util.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""Miscellaneous utility functions used across the application
+"""
+import Const, Env
+import sys, os
+import logging
+import urllib;
+import time;
+import math;
+
+log = logging.getLogger("CDSS")
+
+handler = logging.StreamHandler(sys.stderr)
+formatter = logging.Formatter(Const.LOGGER_FORMAT)
+
+handler.setFormatter(formatter)
+# log.addHandler(handler)
+
+# Global Support variable to monitor how may connections have been created
+numConnections = 0;
+
+def main(argv):
+    """Main method, callable from command line"""
+    pass
+
+if __name__ == "__main__":
+    main(sys.argv)
